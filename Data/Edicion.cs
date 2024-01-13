e3389f800b33d344a78c463d91e07f3148627a2d
Ashley <Ashley@NICOLE>
2024-01-13 10:08:31 -0500

    Add project files.

---
+++ Data/Edicion.cs
@@ -0,0 +1,13 @@
+﻿namespace pruebalaboral.Data
+{
+    public class Edicion
+    {
+        public int id { get; set; }
+        public string todo { get; set; }
+        public bool completed { get; set; }
+        public int userId { get; set; }
+
+        public bool Editando { get; set; } = false;
+        public string TodoEditado { get; set; }
+    }
+}
---
+++ Data/Tareas.cs
@@ -0,0 +1,10 @@
+﻿namespace pruebalaboral.Data
+{
+    public class Tareas
+    {
+        public int id { get; set; }
+        public string todo { get; set; }
+        public bool completed { get; set; }
+        public int userId { get; set; }
+    }
+}
